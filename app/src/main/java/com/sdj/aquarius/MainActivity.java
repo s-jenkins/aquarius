955deeb72b6ed9e4b63f4e486f59aa03bc8da62f
jenkin12temp <simon.jenkins@smithsnews.co.uk>
2017-07-07 09:25:19 +0100

    webview attempt

--- app/src/main/java/com/sdj/aquarius/MainActivity.java
+++ app/src/main/java/com/sdj/aquarius/MainActivity.java
@@ -2,12 +2,22 @@ package com.sdj.aquarius;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+    private ProgressBar progress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        webView = (WebView) findViewById(R.id.wvPortal);
+        webView.loadUrl("file:///aquarius.html");
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
     }
 }
