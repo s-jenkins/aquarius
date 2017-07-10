0257111b0940ca11302ca6fb5f9c44a10406bee4
jenkin12temp <simon.jenkins@smithsnews.co.uk>
2017-07-10 16:20:43 +0100

    experiment with local storage now working

--- app/src/main/assets/www/js/main.js
+++ app/src/main/assets/www/js/main.js
@@ -1,8 +1,8 @@
 function scoreRuns(runs) {
 
     var score = 0;
-    if (localStorage.getItem('score')) {
-        score = localStorage.getItem('score');
+    if (localStorage.getItem('score') && runs > 0) {
+        score = Number(localStorage.getItem('score'));
     }
     score = score + runs;
     document.getElementById("score").innerHTML = score;
